e21145bcd9493a9622e3aea42530b4f2c4e7306d
Peixoto1214 <danpeixoto14@gmail.com>
2022-03-08 18:39:28 -0500

    .

--- Exercises and Homework/Homework 06/starter_code/js/trulia.js
+++ Exercises and Homework/Homework 06/starter_code/js/trulia.js
@@ -7,9 +7,8 @@ let truliaCards = document.getElementsByClassName("trulia-grid-item");
 document.addEventListener('DOMContentLoaded', function(event) {
 
 // Toggle the navigation
-
-
-
+document.querySelector('#trulia-nav-toggle').addEventListener('click',function(e){
+  (e).preventDefault();
 
 
 
@@ -22,9 +21,6 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
       // Add the featured class on the one clicked on
 
+}
 
-
-  });  
-
-
-
+)}; 
