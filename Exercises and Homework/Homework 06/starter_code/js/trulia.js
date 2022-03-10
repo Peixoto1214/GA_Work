c420c564c7101d98ea56eef2d59302752fa1632d
Peixoto1214 <danpeixoto14@gmail.com>
2022-03-10 17:32:37 -0500

    .

--- Exercises and Homework/Homework 06/starter_code/js/trulia.js
+++ Exercises and Homework/Homework 06/starter_code/js/trulia.js
@@ -1,18 +1,50 @@
 
 
-let truliaCards = document.getElementsByClassName("trulia-grid-item");
+
 
 
 
 document.addEventListener('DOMContentLoaded', function(event) {
-
+  let truliaCards = document.querySelectorAll(".trulia-grid-item");
+  console.log(typeof truliaCards)
 // Toggle the navigation
-document.querySelector('#trulia-nav-toggle').addEventListener('click',function(e){
-  (e).preventDefault();
+document.querySelector('.trulia-nav-toggle').addEventListener('click',function(e){
+  e.preventDefault();
 
+  let navShow = document.querySelector('.navShow')
+  navShow.classList.toggle('trulia-nav-mobilehide')
+  console.log(navShow.classList)
+})
 
 
 // Loop through all the cards
+//  truliaCards.forEach(houseCard=>{ 
+
+//   houseCard.addEventListener('click',function(e){
+//     e.preventDefault();
+    
+//     houseCard.classList.remove('trulia-grid-item')
+//     houseCard.classList.add('trulia-featured-grid-item')
+//     console.log(houseCard.classList)
+//   })
+
+
+//  })
+
+for (let index = 0; index < truliaCards.length; index++) {
+  const element = truliaCards[index];
+  element.addEventListener('click', function(){
+  
+    for (let i = 0; i < truliaCards.length; i++) {
+      const el = truliaCards[i];
+      el.classList.remove('trulia-featured-grid-item')
+    }
+    this.classList.add('trulia-featured-grid-item')
+})
+
+}
+
+ 
     
   // Add a click listener on each card
       
@@ -21,6 +53,6 @@ document.querySelector('#trulia-nav-toggle').addEventListener('click',function(e
 
       // Add the featured class on the one clicked on
 
-}
 
-)}; 
+
+});
