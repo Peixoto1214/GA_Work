54c4b5d51e126541cc245b6493cf501254936914
Peixoto1214 <danpeixoto14@gmail.com>
2022-03-12 00:19:32 -0500

    .

---
+++ Exercises and Homework/Homework 04/starter_code/index.js
@@ -0,0 +1,12 @@
+
+
+let score = 0;
+let ballEl = document.querySelector('.js-ball');
+let scoreEl = document.querySelector('.js-score');
+
+
+function declareWinner ()
+    {document.body.classList.add('game-over');
+
+}
+
--- Exercises and Homework/Homework 04/starter_code/level-one.js
+++ Exercises and Homework/Homework 04/starter_code/level-one.js
@@ -5,11 +5,11 @@
 let score = 0;
 let ballEl = document.querySelector('.js-ball');
 let scoreEl = document.querySelector('.js-score');
-
+//add a victory annoucement 
 function declareWinner() {
   document.body.classList.add('game-over');
 }
-
+//increase score with every click inside dot
 ballEl.addEventListener('click', function(){
   score +=10;
 
